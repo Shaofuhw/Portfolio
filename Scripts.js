d93b6cd9218817fd6d4942decee53c91a1204994
Shao Fu <Shao Fu>
2016-11-22 20:27:25 +0100

    Reorganized files

--- Scripts.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	/* Toggle the text when click on Skill Box Button and change icon*/
-	$(".sb-btn-toggle").on("click", function(){
-		$(this).parent().find(".sb-text").fadeToggle("fast", "linear");
-		$(this).find(".glyphicon").toggle();
-	});
-});
